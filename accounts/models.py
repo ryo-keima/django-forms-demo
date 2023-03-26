5a8c445a8a78475e77c8684b25e6771a4f127b8c
ryo-keima <63078940+ryo-keima@users.noreply.github.com>
2023-03-26 13:58:26 +0900

    Update accounts.admin

--- accounts/models.py
+++ accounts/models.py
@@ -6,33 +6,32 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
-    def _create_user(self, email, username, password, **extra_fields):
+    def _create_user(self, email, account_id, password, **extra_fields):
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
+        user = self.model(email=email, account_id=account_id, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        user.full_clean()
 
         return user
 
-    def create_user(self, email, username, password=None, **extra_fields):
+    def create_user(self, email, account_id, password=None, **extra_fields):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(
             email=email,
-            username=username,
+            account_id=account_id,
             password=password,
             **extra_fields,
         )
 
-    def create_superuser(self, email, username, password, **extra_fields):
+    def create_superuser(self, email, account_id, password, **extra_fields):
         extra_fields['is_active'] = True
         extra_fields['is_staff'] = True
         extra_fields['is_superuser'] = True
         return self._create_user(
             email=email,
-            username=username,
+            account_id=account_id,
             password=password,
             **extra_fields,
         )
@@ -40,19 +39,31 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField(
-        verbose_name=_("username"),
+    account_id = models.CharField(
+        verbose_name=_("account_id"),
         unique=True,
-        max_length=150
+        max_length=10
     )
     email = models.EmailField(
         verbose_name=_("email"),
         unique=True
     )
-    age = models.IntegerField(
-        verbose_name=_("age"),
+    first_name = models.CharField(
+        verbose_name=_("first_name"),
+        max_length=150,
         null=True,
-        blank=True
+        blank=False
+    )
+    last_name = models.CharField(
+        verbose_name=_("last_name"),
+        max_length=150,
+        null=True,
+        blank=False
+    )
+    birth_date = models.DateField(
+        verbose_name=_("birth_date"),
+        blank=True,
+        null=True
     )
     is_superuser = models.BooleanField(
         verbose_name=_("is_superuer"),
@@ -66,11 +77,18 @@ class User(AbstractBaseUser, PermissionsMixin):
         _('active'),
         default=True,
     )
+    created_at = models.DateTimeField(
+        verbose_name=_("created_at"),
+        auto_now_add=True)
+    updated_at = models.DateTimeField(
+        verbose_name=_("updateded_at"),
+        auto_now=True
+    )
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'account_id'
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
-        return self.username
+        return self.account_id
